df1020bd286e8e0c7b3f37567fd0ecfb2da27993
timi <timi546d69@163.com>
2023-10-11 20:44:18 +0800

    complete proj1a

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -17,11 +17,14 @@ public class LinkedListDeque <T>{
     public void addFirst(T item){
         if(item!=null){
             ListNode node=new ListNode(item);
-            node.next=this.first;
-            this.first.front=node;
-            this.first=node;
-            if(this.size==0)
+            if(this.size==0){
+                this.first=node;
                 this.last=this.first;
+            }else {
+                node.next=this.first;
+                this.first.front=node;
+                this.first=node;
+            }
             this.size++;
         }
     }
@@ -34,6 +37,7 @@ public class LinkedListDeque <T>{
                 this.first=this.last;
             }else{
                 this.last.next=node;
+                node.front=this.last;
                 this.last=node;
             }
             this.size++;
@@ -52,6 +56,7 @@ public class LinkedListDeque <T>{
             ListNode index=this.first;
             while (index!=this.last){
                 System.out.print(index.item+" ");
+                index=index.next;
             }
             System.out.print(index.item);
         }
@@ -61,9 +66,14 @@ public class LinkedListDeque <T>{
             return null;
         else {
             ListNode result=this.first;
-            if(this.size==1)
+            if(this.size==1){
+                this.first=null;
                 this.last=null;
-            this.first=this.first.next;
+            }else {
+                this.first=this.first.next;
+                this.first.front=null;
+            }
+
             this.size--;
             return result.item;
         }
@@ -77,23 +87,36 @@ public class LinkedListDeque <T>{
                 this.first=null;
                 this.last=null;
             }else {
-
+                this.last=this.last.front;
+                this.last.next=null;
             }
             this.size--;
             return result.item;
         }
     }
     public T get(int index){
-
+        if(index<0||index>=this.size) return null;
+        int i=0;
+        ListNode ind=this.first;
+        while(ind!=null){
+            if(i==index)
+                return ind.item;
+            i++;
+            ind=ind.next;
+        }
+        return null;
     }
     public LinkedListDeque(){
         this.first=null;
         this.last=null;
         this.size=0;
     }
-
-
     public T getRecursive(int index){
-
+        if(index<0||index>=this.size) return null;
+        return  recurisive(index).item;
+    }
+    public ListNode recurisive(int index){
+        if(index==0) return this.first;
+        return recurisive(index-1).next;
     }
 }
